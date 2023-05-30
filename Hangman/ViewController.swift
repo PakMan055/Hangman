63236d8f6c9005dad3dc5be83745458cf34e453b
Roman Pak <r.v.pak@mail.ru>
2023-05-30 21:45:42 +0600

    Initial Commit

---
+++ Hangman/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Hangman
+//
+//  Created by Roman Pak on 30.05.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
